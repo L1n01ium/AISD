1d79b31440f080c72c52796e86546216d13454f2
L1n01ium <znadzafov2007@gmail.com>
2026-03-05 21:20:49 +0300

    Delete HomeWork2 directory

--- HomeWork2/ClassWork2702.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class ClassWork2702 {
-    public static void main(String[] args) {
-        Scanner mysc = new Scanner(System.in);
-        String str = mysc.nextLine();
-
-    }
-}
--- HomeWork2/Node.java
+++
@@ -1,9 +0,0 @@
-public class Node {
-    int value;
-    Node next;
-
-    Node(int value) {
-        this.value = value;
-        this.next = null;
-    }
-}
